64062a5075cc1866fbe98a50a91158065eeb5de4
simbyungki <dung8524@gmail.com>
2019-02-19 09:42:54 +0900

    Updates

---
+++ precache-manifest.e2cc6cd298a19cfa9c1a85078258d5e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e3d07953dfb554cfb69",
+    "url": "/movie-app/static/js/runtime~main.6e3d0795.js"
+  },
+  {
+    "revision": "25400d81b4919d64a7fa",
+    "url": "/movie-app/static/js/main.25400d81.chunk.js"
+  },
+  {
+    "revision": "f2fffbf57852fbf371ee",
+    "url": "/movie-app/static/js/2.f2fffbf5.chunk.js"
+  },
+  {
+    "revision": "25400d81b4919d64a7fa",
+    "url": "/movie-app/static/css/main.3b38d8aa.chunk.css"
+  },
+  {
+    "revision": "f5cc1acf78bbe78fb4c8472590672678",
+    "url": "/movie-app/index.html"
+  }
+];
